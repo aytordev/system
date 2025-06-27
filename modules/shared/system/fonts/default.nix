b3074c13dc1b6832f81eec8ac78620bf2d0e35b9
Aytor <me@aytor.dev>
2025-06-27 16:16:11 +0200

    :recycle: refactor(fonts): extract shared fonts module and update darwin implementation
    - Created new shared fonts module with common font configurations
    - Refactored darwin fonts module to use shared implementation
    - Removed redundant font package definitions from darwin module
    - Updated imports and fixed mk* function imports from lib
    - Removed enable flag from fonts configuration

---
+++ modules/shared/system/fonts/default.nix
@@ -0,0 +1,61 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) types mkOption;
+
+  # Common fonts that should be available on all systems
+  commonFonts = with pkgs; [
+    # Desktop Fonts
+    corefonts  # MS fonts
+    b612  # High legibility
+    material-icons
+    material-design-icons
+    work-sans
+    comic-neue
+    source-sans
+    inter
+    lexend
+
+    # Emojis
+    noto-fonts-color-emoji
+    noto-fonts-monochrome-emoji
+
+    # Nerd Fonts
+    cascadia-code
+    monaspace
+    nerd-fonts.symbols-only
+  ];
+
+in {
+  options.system.fonts = with types; {
+    fonts = mkOption {
+      type = listOf package;
+      default = commonFonts;
+      description = "List of font packages to install system-wide";
+    };
+      
+    default = mkOption {
+      type = str;
+      default = "MonaspaceNeon";
+      description = "Default system font name";
+    };
+      
+    size = mkOption {
+      type = addCheck int (n: n > 0);
+      default = 13;
+      description = "Default font size in points";
+    };
+  };
+
+  config = {
+    environment.variables = {
+      # Enable icons in tooling since we have nerdfonts
+      LOG_ICONS = "true";
+    };
+  };
+}
