8bc4a281d2bb7777b9ff5c052be6a65dd6715692
aytordev <me@aytor.dev>
2025-07-08 19:53:20 +0200

    :sparkles: feat(darwin): add user module with secrets integration
    - Add new darwin user module with configurable options
    - Integrate with inputs.secrets for secure credential management
    - Support user name, email, full name, and UID configuration
    - Set default shell to zsh for all users

---
+++ modules/darwin/user/default.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, inputs, ... }:
+
+let
+  inherit (lib) types mkEnableOption mkOption;
+  
+  cfg = config.darwin.user;
+in {
+  options.darwin.user = {
+    enable = mkEnableOption "user configuration";
+
+    name = mkOption {
+      type = types.str;
+      default = inputs.secrets.username;
+      description = "The username for the user account.";
+    };
+
+    email = mkOption {
+      type = types.str;
+      default = inputs.secrets.useremail;
+      description = "The email address of the user.";
+    };
+
+    fullName = mkOption {
+      type = types.str;
+      default = inputs.secrets.userfullname;
+      description = "The full name of the user.";
+    };
+
+    uid = mkOption {
+      type = types.nullOr types.int;
+      default = 501;
+      description = "The UID for the user account. Set to null for automatic assignment.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.users.${inputs.secrets.username} = {
+      inherit (cfg) uid;
+      home = "/Users/${inputs.secrets.username}";
+      shell = pkgs.zsh;
+      description = inputs.secrets.userfullname;
+    };
+  };
+}
