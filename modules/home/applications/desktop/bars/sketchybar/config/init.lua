17c1d3b6f7efaddf5f2fa08b42eeaa518d27f709
aytordev <me@aytor.dev>
2025-08-04 19:06:31 +0200

    :recycle: refactor(sketchybar): reorganize configuration structure
    - Reorganize configuration files into a clearer structure
    - Remove obsolete and duplicate files
    - Update references in configuration files

--- modules/home/applications/desktop/bars/sketchybar/config/init.lua
+++ modules/home/applications/desktop/bars/sketchybar/config/init.lua
@@ -1,15 +1,11 @@
-#!/usr/bin/env lua
-package.path = package.path .. ";./lua/?.lua" .. ";./lua/?/init.lua" .. ";./scripts/?.lua"
+sbar = require("sketchybar")
 
-Sbar = require("sketchybar")
-
-Sbar.begin_config()
-Sbar.hotload(true)
+sbar.begin_config()
+sbar.hotload(true)
 
 require("bar")
 require("default")
 require("items")
 
-Sbar.end_config()
-
-Sbar.event_loop()
+sbar.end_config()
+sbar.event_loop()
