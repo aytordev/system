2cbc82857fb45fd7fecfc5264a9cba14a4c124ff
aytordev <me@aytor.dev>
2025-10-04 11:17:17 +0200

    :arrow_up: feat(zed): switch to unstable package for latest features
    - Change from pkgs-stable.zed-editor to pkgs.zed-editor
    - Use unstable Nix packages to get latest Zed editor version
    - Ensures access to newest features and bug fixes
    
    Refs editor configuration update

--- modules/home/applications/desktop/editors/zed/default.nix
+++ modules/home/applications/desktop/editors/zed/default.nix
@@ -16,7 +16,7 @@ in {
   config = mkIf cfg.enable {
     programs.zed-editor = {
       enable = true;
-      package = pkgs-stable.zed-editor;
+      package = pkgs.zed-editor;
 
       # Extensions - https://github.com/zed-industries/extensions/tree/main/extensions
       extensions = [
