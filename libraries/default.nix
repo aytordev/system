98a2299658d919b353961ae78fff86b52a18f6e0
aytordev <me@aytor.dev>
2026-01-04 00:56:53 +0100

    :recycle: refactor(libraries): export via flake.lib
    - Rewrite libraries/default.nix for flake-parts compatibility
    - Export file, module, system namespaces
    - Direct access to mkDarwin, mkHome, enabled, disabled
    - Integrate all new library extensions

--- libraries/default.nix
+++ libraries/default.nix
@@ -1,48 +1,62 @@
-{lib, ...} @ args: let
-  getNixFiles = dir: let
-    allFiles = builtins.attrNames (builtins.readDir dir);
-    isNixFileOrDir = file: let
-      isNixFile = lib.strings.hasSuffix ".nix" file;
-      isNotDefault = file != "default.nix";
-      isDir = (builtins.readDir dir)."${file}" == "directory";
-    in
-      (isNixFile && isNotDefault) || isDir;
-    filteredFiles = builtins.filter isNixFileOrDir allFiles;
-    processSubdir = file:
-      if (builtins.readDir dir)."${file}" == "directory"
-      then let
-        subdir = dir + "/${file}";
-        subFiles = getNixFiles subdir;
-      in
-        builtins.map (f: "${file}/${f}") subFiles
-      else [];
-    subdirFiles = builtins.concatMap processSubdir filteredFiles;
-    allNixFiles = filteredFiles ++ subdirFiles;
-  in
-    allNixFiles;
-  importFile = file: let
-    filePath = ./. + "/${file}";
-    imported = import filePath args;
-    result =
-      if builtins.isFunction imported
-      then imported args
-      else if builtins.isAttrs imported
-      then imported
-      else throw "" "
-            Error in ${file}:
-            Imported file must return an attribute set or function that returns an attribute set.
-            Got: ${builtins.typeOf imported}
-          " "";
-  in
-    result;
-  importedModules = map importFile (getNixFiles ./.);
-  combinedModules = lib.foldl' lib.recursiveUpdate {} importedModules;
-  publicApi = {
-    inherit (combinedModules) relativeToRoot macosSystem;
-    inherit lib;
-    _internal = {
-      inherit getNixFiles importFile;
-    };
-  };
+{ inputs, ... }:
+let
+  inherit (inputs.nixpkgs) lib;
+  self = inputs.self or ../.;
+
+  # Import library modules
+  file = import ./file { inherit inputs self; };
+  module = import ./module { inherit lib; };
+  system = import ./system { inherit inputs; };
+  relativeToRoot = (import ./relative-to-root { inherit lib; }).relativeToRoot;
+  overlay = import ./overlay { inherit inputs; };
 in
-  combinedModules // publicApi
+{
+  # Export as flake-parts module
+  flake.lib = {
+    # File utilities
+    inherit (file)
+      readFile
+      pathExists
+      safeImport
+      scanDir
+      getFile
+      getNixFiles
+      mergeAttrs
+      importFiles
+      importDir
+      importModulesRecursive
+      parseSystemConfigurations
+      parseHomeConfigurations
+      filterNixOSSystems
+      filterDarwinSystems
+      ;
+
+    # Path utilities
+    inherit relativeToRoot;
+
+    # Re-export with namespaces for direct access
+    file = file;
+    module = module;
+    system = system;
+    overlay = overlay;
+
+    # System builders (also directly accessible)
+    inherit (system) mkDarwin mkSystem mkHome common;
+
+    # Module utilities (also directly accessible)
+    inherit (module)
+      mkOpt
+      mkOpt'
+      mkBoolOpt
+      mkBoolOpt'
+      enabled
+      disabled
+      capitalize
+      boolToNum
+      default-attrs
+      force-attrs
+      nested-default-attrs
+      nested-force-attrs
+      ;
+  };
+}
