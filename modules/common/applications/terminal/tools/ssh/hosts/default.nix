8d5a3dfed6c90b54c93d3cf813c6cea81eceb085
aytordev <me@aytor.dev>
2026-01-04 13:35:26 +0100

    :recycle: refactor(ssh): update hosts with wang-lin configuration

---
+++ modules/common/applications/terminal/tools/ssh/hosts/default.nix
@@ -0,0 +1,10 @@
+{
+  "wang-lin" = {
+    hostname = "wang-lin.local";
+    username = "aytordev";
+    system = "darwin";
+    gpgAgent = true;
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUV4bxRgp4WZX0MkVv7jg9q2i44yE6jUnnitMDGb0mO";
+    userPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C+C"; # Placeholder: Update with actual user public key
+  };
+}
