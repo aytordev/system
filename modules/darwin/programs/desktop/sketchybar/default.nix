20cd430b3354a8b44992b93a67097bdda1dae487
aytordev <me@aytor.dev>
2026-01-05 13:17:23 +0100

    :sparkles: feat(darwin): add new suites and programs

---
+++ modules/darwin/programs/desktop/sketchybar/default.nix
@@ -0,0 +1,57 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkIf mkOption types;
+
+  cfg = config.aytordev.programs.desktop.bars.sketchybar;
+  userHome = config.users.users.${config.aytordev.user.name}.home;
+in
+
+{
+  options.aytordev.programs.desktop.bars.sketchybar = {
+    enable = lib.mkEnableOption "sketchybar log rotation";
+
+    logPaths = {
+      stdout = mkOption {
+        type = types.str;
+        default = "${userHome}/Library/Logs/sketchybar/sketchybar.out.log";
+        description = "Path to sketchybar stdout log file";
+      };
+
+      stderr = mkOption {
+        type = types.str;
+        default = "${userHome}/Library/Logs/sketchybar/sketchybar.err.log";
+        description = "Path to sketchybar stderr log file";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    system.newsyslog.files.sketchybar = [
+      {
+        logfilename = cfg.logPaths.stdout;
+        mode = "644";
+        owner = config.aytordev.user.name;
+        group = "staff";
+        count = 7;
+        size = "2048";
+        flags = [
+          "Z"
+          "C"
+        ];
+      }
+      {
+        logfilename = cfg.logPaths.stderr;
+        mode = "644";
+        owner = config.aytordev.user.name;
+        group = "staff";
+        count = 7;
+        size = "2048";
+        flags = [
+          "Z"
+          "C"
+        ];
+      }
+    ];
+  };
+}
