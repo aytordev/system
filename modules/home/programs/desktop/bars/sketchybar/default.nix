5e9b426f015457faecec380d33038eaa8cab4413
aytordev <me@aytor.dev>
2026-01-18 01:20:31 +0100

    :recycle: refactor(sketchybar): move luaposix to packages

--- modules/home/programs/desktop/bars/sketchybar/default.nix
+++ modules/home/programs/desktop/bars/sketchybar/default.nix
@@ -50,30 +50,6 @@
       command mas "$@" && ${getExe cfg.package} --trigger brew_update
     }
   '';
-  # Custom luaposix build
-  luaposix = pkgs.lua54Packages.buildLuarocksPackage {
-    pname = "luaposix";
-    version = "36.2.1";
-    # Use git source to avoid src.rock directory structure issues
-    src = pkgs.fetchFromGitHub {
-      owner = "luaposix";
-      repo = "luaposix";
-      rev = "v36.2.1";
-      hash = "sha256-oxHH7RmaEGLU1tSlFhtf7F6CKOSRaNamq7QxtWyfwtI=";
-    };
-    knownRockspec = (pkgs.fetchurl {
-      url = "https://luarocks.org/manifests/gvvaughan/luaposix-36.2.1-1.rockspec";
-      hash = "sha256-mlv8WUAdD+pfMUXGVh3zGgknfMoKDzFcyoeOyEtJj1Y=";
-    }).outPath;
-
-    # Fix rockspec to assume we are at root
-    preBuild = ''
-      # The rockspec expects to be able to run build-aux/luke
-      # Check if we are in the right directory
-      ls -la
-    '';
-    disabled = false;
-  };
 in {
   options.aytordev.programs.desktop.bars.sketchybar = {
     enable = mkEnableOption "Sketchybar status bar";
@@ -106,7 +82,7 @@ in {
       extraPackages = allPackages;
 
       extraLuaPackages = luaPkgs: [
-        luaposix
+        pkgs.aytordev.luaposix
         luaPkgs.dkjson
         luaPkgs.lua-cjson
       ];
