71f3441698ea73719519e556560f5e101200051d
aytordev <me@aytor.dev>
2026-08-22 14:19:28 +0200

    fix(lib): repair module helper contracts

--- tests/default.nix
+++ tests/default.nix
@@ -3,6 +3,30 @@
   lib,
 }: let
   inherit (self.lib) file module;
+  evaluateGeneratedModule = enable:
+    (lib.evalModules {
+      modules = [
+        (module.mkModule {
+          name = "example";
+          description = "example module";
+          options.message = lib.mkOption {
+            type = lib.types.str;
+            default = "default";
+          };
+          config.testResult = "applied";
+        })
+        ({lib, ...}: {
+          options.testResult = lib.mkOption {
+            type = lib.types.str;
+            default = "disabled";
+          };
+          config.aytordev.example = {
+            inherit enable;
+            message = "custom";
+          };
+        })
+      ];
+    }).config;
 in {
   testBoolToNumTrue = {
     expr = module.boolToNum true;
@@ -35,5 +59,28 @@ in {
   testMkOptDefault = {
     expr = (module.mkOpt' lib.types.int 5).default;
     expected = 5;
+  };
+
+  testEnablePreservesImports = {
+    expr = (module.enable "primary" {imports = ["additional"];}).imports;
+    expected = [
+      "primary"
+      "additional"
+    ];
+  };
+
+  testGeneratedModuleDisabled = {
+    expr = (evaluateGeneratedModule false).testResult;
+    expected = "disabled";
+  };
+
+  testGeneratedModuleEnabled = {
+    expr = (evaluateGeneratedModule true).testResult;
+    expected = "applied";
+  };
+
+  testGeneratedModuleCustomOptions = {
+    expr = (evaluateGeneratedModule true).aytordev.example.message;
+    expected = "custom";
   };
 }
