70471e3b1a503d3c6e03f39209822714efac90d3
aytordev <me@aytor.dev>
2026-08-22 15:36:13 +0200

    fix(ssh): require explicit key-only access

--- modules/darwin/services/openssh/default.nix
+++ modules/darwin/services/openssh/default.nix
@@ -4,11 +4,22 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf mkOption types;
+  inherit
+    (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
   cfg = config.aytordev.services.openssh;
 in {
   options.aytordev.services.openssh = {
     enable = mkEnableOption "OpenSSH service";
+    authorizedKeys = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Public keys permitted to authenticate as the primary user.";
+    };
     extraConfig = mkOption {
       type = types.lines;
       default = "";
@@ -26,10 +37,30 @@ in {
   };
 
   config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = cfg.authorizedKeys != [];
+        message = "aytordev.services.openssh requires at least one authorized key";
+      }
+    ];
+
     environment.systemPackages = [pkgs.openssh];
     services.openssh = {
       enable = true;
-      inherit (cfg) extraConfig;
+      extraConfig = ''
+        AuthenticationMethods publickey
+        KbdInteractiveAuthentication no
+        LoginGraceTime 30
+        MaxAuthTries 3
+        PasswordAuthentication no
+        PermitEmptyPasswords no
+        PermitRootLogin no
+        PubkeyAuthentication yes
+        X11Forwarding no
+        ${cfg.extraConfig}
+      '';
     };
+
+    users.users.${config.aytordev.user.name}.openssh.authorizedKeys.keys = cfg.authorizedKeys;
   };
 }
