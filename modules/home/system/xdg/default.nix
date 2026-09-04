ed6709fba3a00cdee38159b2ab1ff749b17a3c6f
aytordev <me@aytor.dev>
2026-09-04 16:37:18 +0200

    refactor(home-xdg): convert to namespaced xdg capability

--- modules/home/system/xdg/default.nix
+++ modules/home/system/xdg/default.nix
@@ -1,3 +1,20 @@
-_: {
-  config.xdg.enable = true;
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf mkOption types;
+  cfg = config.aytordev.system.xdg;
+in {
+  options.aytordev.system.xdg = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable XDG base directories";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    xdg.enable = true;
+  };
 }
